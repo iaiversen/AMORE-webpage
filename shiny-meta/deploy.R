8739d3250b4c124572a1ca3eaf83477991c8b161
iaiversen <iaiversen@hotmail.com>
2025-10-28 17:04:19 +0100

    Secure credentials in Deploy.R

--- shiny-meta/deploy.R
+++ shiny-meta/deploy.R
@@ -1,19 +1,26 @@
 library(rsconnect)
 
+# Read credentials from environment variables
 rsconnect::setAccountInfo(
-  name = "meta-oxytocin",
-  token = "5407112F282F857A8E250D7B886D4D3D",  
-  secret = "+C9pcd5UszwYCh9vqSvxUJmxZOwM4/qq8UE97fOh"  
+  name = Sys.getenv("SHINYAPPS_ACCOUNT"),
+  token = Sys.getenv("SHINYAPPS_TOKEN"),
+  secret = Sys.getenv("SHINYAPPS_SECRET")
 )
+
+# Check if credentials are loaded
+if(Sys.getenv("SHINYAPPS_ACCOUNT") == "") {
+  stop("Credentials not found. Create .Renviron file with:\n",
+       "SHINYAPPS_ACCOUNT=your_account\n",
+       "SHINYAPPS_TOKEN=your_token\n",
+       "SHINYAPPS_SECRET=your_secret")
+}
 
 rsconnect::deployApp(
   appName = "shiny-meta",
-  account = "meta-oxytocin",
+  account = Sys.getenv("SHINYAPPS_ACCOUNT"),
   server = "shinyapps.io",
   forceUpdate = TRUE
 )
 
 # Check rsconnect accounts
 rsconnect::accountInfo()
-
-
